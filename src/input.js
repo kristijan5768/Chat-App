a9ad63cd1887e62cb45978af2b2eb8357429ecbc
kristijan5768 <kristijan5768@gmail.com>
2023-03-23 19:44:04 +0100

    fourth commit

--- src/input.js
+++ src/input.js
@@ -1,32 +1,49 @@
-import { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
+
 class Input extends Component {
     state = {
         text: '',
+        charCount: 0,
+        showError: false,
     };
 
-    onChange(e) {
-        this.setState({ text: e.target.value });
-    }
+    onChange = (e) => {
+        const text = e.target.value;
+        const charCount = text.length;
+        const showError = charCount > 120;
+        this.setState({ text, charCount, showError });
+    };
 
-    onSubmit(e) {
+    onSubmit = (e) => {
         e.preventDefault();
-        this.setState({ text: '' });
-        this.props.onSendMessage(this.state.text);
-    }
+        if (this.state.charCount <= 120) {
+            this.props.onSendMessage(this.state.text);
+            this.setState({ text: '', charCount: 0, showError: false });
+        } else {
+            this.setState({ showError: true });
+        }
+    };
+
     render() {
         return (
             <div className="Input">
-                <form onSubmit={(e) => this.onSubmit(e)}>
+                <form onSubmit={this.onSubmit}>
                     <input
-                        onChange={(e) => this.onChange(e)}
+                        onChange={this.onChange}
                         value={this.state.text}
                         type="text"
                         placeholder="Enter your message and press ENTER!"
                         autoFocus={true}
+                        maxLength="121"
                     />
-                    <button class="button">Send</button>
+                    <button className="button">Send</button>
                 </form>
+                {this.state.showError && (
+                    <div className="error">
+                        Character limit exceeded!(Max. characters per message
+                        120)
+                    </div>
+                )}
             </div>
         );
     }
