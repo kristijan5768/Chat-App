5ed08a9df5c11104d27851e9b1a9c6abea7a520b
kristijan5768 <kristijan5768@gmail.com>
2023-04-23 16:27:58 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Messages from './components/Messages';
 import Input from './components/Input';
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ScrollableFeed from 'react-scrollable-feed';
 import dayjs from 'dayjs';
 
